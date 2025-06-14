49a9da189bceb9d28862681896c4c291594f94c8
abid4850 <abidhussainnoul512@gmail.com>
2025-06-14 20:32:09 +0500

    streamlit_app

--- 09_streamlit_app/04_wordcloud_app/word_cloud_app.py
+++ 09_streamlit_app/04_wordcloud_app/word_cloud_app.py
@@ -123,7 +123,3 @@ if uploaded_file:
     if st.button('Download Word Count Table as CSV'):
         st.markdown(get_table_download_link(word_count, "word_count.csv", "Click Here to Download"), unsafe_allow_html=True)
 
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-ax.plot(...)
-st.pyplot(fig)
